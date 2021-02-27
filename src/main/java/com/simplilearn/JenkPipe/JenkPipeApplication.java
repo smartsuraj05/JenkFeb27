2893ee094df519a3306ae9a6004e6ac710d8cc87
student <smartsuraj05@gmail.com>
2021-02-27 17:13:55 +0000

    new files

---
+++ src/main/java/com/simplilearn/JenkPipe/JenkPipeApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/JenkPipe/JenkPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
